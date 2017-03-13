062f837bd26965e9b56bafd8b717aaeecdcaec4e
ssw3131 <ssw3131@daum.net>
2017-03-13 16:59:27 +0900

    setting

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require( 'gulp' );
+var concat = require( 'gulp-concat' );
+var sourcemaps = require( 'gulp-sourcemaps' );
+
+var src = 'public/src/';
+var dist = 'public/dist/';
+
+gulp.task( "combine", function() {
+	return gulp.src( [
+			src + 'bloodstone.js',
+			src + 'detector.js',
+			src + 'util.js'
+		] )
+		.pipe(sourcemaps.init())
+		.pipe( concat( 'bloodstone.js' ) )
+		.pipe(sourcemaps.write('./maps'))
+		.pipe( gulp.dest( dist ) );
+} );
+
+gulp.task( 'default', [
+	'combine'
+] );
